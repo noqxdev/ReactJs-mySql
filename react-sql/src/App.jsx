f6d1cb8256e54d0cec5091beeefff0a28ad683f7
noqx <noel.merz09@outlook.com>
2025-02-21 15:32:00 +0100

    refactor: integrate MUI DataGrid for user data display and apply dark theme

--- react-sql/src/App.jsx
+++ react-sql/src/App.jsx
@@ -1,20 +1,60 @@
-import { useState } from 'react'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import mySqlLogo from './assets/mysql-official.svg'
+import { DataGrid } from '@mui/x-data-grid';
+import Box from '@mui/material/Box';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
 import './App.css'
+
+function DataGridDemo({ rows }) {
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'username', headerName: 'Username', width: 150 },
+    { field: 'email', headerName: 'Email', width: 200 },
+  ];
+
+  return (
+    <Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </Box>
+  );
+}
 
 function App() {
   const [data, setData] = useState([]);
   useEffect(() => {
-    fetch('http://localhost:8081/users') // Backend server link 'http://localhost:8081/... 
+    fetch('http://localhost:8081/users')
       .then(res => res.json())
       .then(data => setData(data))
       .catch(err => console.log(err));
   }, [])
+
+  const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
   return (
     <>
+      <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <div>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
@@ -26,29 +66,11 @@ function App() {
           <img src={mySqlLogo} className="logo" alt="MySQL logo" />
         </a>
       </div>
-      <h1>React + Vite | MySQL</h1>
       <div className="card"></div>
       <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Username</th>
-              <th>email</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((d, i) => (
-              <tr key={i}>
-                <td>{d.id}</td>
-                <td>{d.username}</td>
-                <td>{d.email}</td>
-              </tr>
-            
-            ))}
-          </tbody>
-        </table>
       </div>
+        <DataGridDemo rows={data} />
+           </ThemeProvider>
     </>
   )
 }
